465279ecf54e706f7b41758503d7d54196e2daa3
Cassoli <fernandes.cassoli@gmail.com>
2024-02-04 19:15:13 -0300

    first commit

---
+++ src/main/java/mypact/operadoresternario/OperadoresTernario.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package mypact.operadoresternario;
+
+/**
+ *
+ * @author Pessoal
+ */
+public class OperadoresTernario {
+
+    public static void main(String[] args) {
+        int n1,n2,r;
+        n1=14;
+        n2=18;
+        r = (n1>n2)?n1+n2:n1-n2;
+        System.out.println(r);
+    }
+}
